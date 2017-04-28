65bf750e810449e8736ae1f1ae7e24334c676ccb
Shiny Yang <syang@sm-syang1.roblox.local>
2017-04-28 15:13:47 -0700

    test

---
+++ content/blogpost/post18.php
@@ -0,0 +1,109 @@
+<h1>Robeats</h1>
+<p>Posted by <b>SPOTCO</b> on April 2017</p>
+<hr/>
+
+Working on a MMO rhythm game (tentatively called &quot;Robeats&quot;) on Roblox for a few months.<br/>
+I wanted to do a rhythm game done right - social, &quot;Roblox-y&quot; and lots of retention/monetization hooks. <br/>
+There have been a couple other rhythm games done on Roblox (like <a href="https://www.roblox.com/games/290276263/Rhythm-Master">Rhythm Master</a> 
+by Xsitsu).
+None of them have been particularily successful either in monetization or popularity - this is something I hope to change. <br/>
+<br/>
+My general idea is to something similar to the successful (in Japan) Idolmaster/Love Live mobile games:<br/>
+Simple rhythm gameplay as the core, supported by RPG-ish progression.<br/>
+My goal is to also have a ELO grind as the "core game". This is a model that's been shown to work (Clash of Clans, Hearthstone, etc) but hasn't really "happened" in Roblox yet.
+
+<br/>
+<br/>
+
+Since starting, I've been regularly posting devlog updates on twitter.
+<br/>
+<hr/>
+<br/>
+
+It all started as a hackweek project. Here's what we had after a week.
+<iframe width="560" height="315" src="https://www.youtube.com/embed/VTydYUjV9V0" frameborder="0" allowfullscreen></iframe>
+
+<br/>
+
+Worked on gameplay polish over Christmas vacation.
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">more UX work <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/PpMRo4NWoA">pic.twitter.com/PpMRo4NWoA</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/814750530652098560">December 30, 2016</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Early lobby concepts:
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">super early blocked-out city for scale/sizing by <a href="https://twitter.com/JoshWongArt">@JoshWongArt</a>. how&#39;s this compared to the concept art? <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/Tj6X4kVMYh">pic.twitter.com/Tj6X4kVMYh</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/817292586834489344">January 6, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Early lobby work:
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">another lobby work in progress shot <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/iwMtRVTN8C">pic.twitter.com/iwMtRVTN8C</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/820143283825307648">January 14, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Central tower work:
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">new building design by <a href="https://twitter.com/JoshWongArt">@JoshWongArt</a> , looking amazing! <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/ArdEDOdw9n">pic.twitter.com/ArdEDOdw9n</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/824876508309123072">January 27, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Arena
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">beautiful retractable-dome environment by <a href="https://twitter.com/JoshWongArt">@JoshWongArt</a>, wait till you see it in action <a href="https://twitter.com/hashtag/RobloxDev?src=hash">#RobloxDev</a> <a href="https://t.co/x5yvpIJ4Rw">pic.twitter.com/x5yvpIJ4Rw</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/827799088280199168">February 4, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+More gameplay work and re-adding 2D crowd:
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Added crowds back in. They&#39;ll dance in sync when in FEVER mode. Loads from your friends &amp; very performant now :) <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/N4VW8bEACu">pic.twitter.com/N4VW8bEACu</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/833887394558406656">February 21, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Park + city
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Cityscape. Pretty similar to the concept art. <a href="https://twitter.com/JoshWongArt">@JoshWongArt</a> <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/H7H8q0Mp9v">pic.twitter.com/H7H8q0Mp9v</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/842638838769971200">March 17, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+City/park work
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">bunch more cityscape improvements by <a href="https://twitter.com/JoshWongArt">@JoshWongArt</a> , explorable soon! <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/u1PhnVK8po">pic.twitter.com/u1PhnVK8po</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/845837230631370752">March 26, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Had <a href="https://twitter.com/StormCrystalite">Zappy00</a> come on and do some dance animations + song mappings.
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">New map by <a href="https://twitter.com/StormCrystalite">@StormCrystalite</a> - Pop Culture by Madeon <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/PqfZl1GnSF">pic.twitter.com/PqfZl1GnSF</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/852233338186211328">April 12, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Working on new UI styles.
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Working on new UI reskins by tom. You&#39;ll be seeing these ingame soon! <a href="https://twitter.com/hashtag/ROBLOXDev?src=hash">#ROBLOXDev</a> <a href="https://t.co/coPgaKBO5E">pic.twitter.com/coPgaKBO5E</a></p>&mdash; spotco (@spotco) <a href="https://twitter.com/spotco/status/857481817586188288">April 27, 2017</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<br/>
+
+Still in progress. Hopefully done (a v1 release) by the end of the summer.<br/>
+Thanks to: Josh (2D and 3D art), Tom (2D UI work) and zappy00 (animations and song mapping).
+
+<?php
+function show_image($url,$imgurl,$width,$style="") {
+	?>
+	<a href="<?=$url?>">
+	<img class="alignnone size-medium wp-image-427820 img-polaroid" style="<?=$style?>" src="<?=$imgurl?>" alt="<?=$imgurl?>" width="<?=$width?>"/>
+	</a>
+	<?php
+}
+?>
